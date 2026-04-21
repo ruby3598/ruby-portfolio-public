8ec07a59d8e3b32d4858eb44c4892283cfb1719e
ruby3598 <141223630+ruby3598@users.noreply.github.com>
2026-04-21 12:30:17 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Portfolio from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Portfolio />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
